d209dd3c8c99aa6ac2ba70524d1c920740a6afb0
Nikolay Dimitrov <nid@milestone.dk>
2024-04-05 17:59:40 +0300

    PixelClusterizer - a naive implementation

--- Enscape.Integration.CodingChallenge/IPixelClusterizer.cs
+++ Enscape.Integration.CodingChallenge/IPixelClusterizer.cs
@@ -22,6 +22,57 @@ public class PixelClusterizer : IPixelClusterizer
 {
     public PixelCluster[] CreateClusters(IPixelGrid inputGrid)
     {
-        throw new NotImplementedException();
-    }
+        var unmergedClusters = Enumerable.Range(0, inputGrid.Width)
+            .SelectMany(i => Enumerable.Range(0, inputGrid.Height)
+                .Select(j => (i, j)))
+            .Where(p => inputGrid.Get(p.i, p.j))
+            .Select(p => new PixelCluster(new[] { p }))
+            .ToList();
+
+        var result = new List<PixelCluster>();
+
+        while(unmergedClusters.Any())
+        {
+            var candidate = unmergedClusters.Last(); // just pick one
+            unmergedClusters.Remove(candidate);
+         
+            var adjacent = unmergedClusters
+                .Where(candidate.IsAdjacentTo)
+                .ToArray();
+
+            if (adjacent.Any())
+            {
+                unmergedClusters = unmergedClusters.Except(adjacent).ToList();
+                var union = candidate
+                    .Singleton()
+                    .Concat(adjacent)
+                    .Join();
+                unmergedClusters.Add(union);
+            }
+            else
+            {
+                result.Add(candidate);
+            }
+        }
+
+        return result.ToArray();
+    }    
+}
+
+internal static class Extensions
+{
+    public static IEnumerable<T> Singleton<T>(this T elem) =>
+        Enumerable.Repeat(elem, 1);
+
+    public static PixelCluster Join(this IEnumerable<PixelCluster> clusters) =>
+       new PixelCluster(clusters.SelectMany(cl => cl.Pixels).ToArray());
+
+    public static bool IsAdjacentTo(this PixelCluster a, PixelCluster b) =>
+        a.Pixels.Any(p1 => b.Pixels.Any(p2 => p1.IsAdjacentTo(p2)));
+
+    public static bool IsAdjacentTo(this (int x, int y) p1, (int x, int y) p2) =>
+        p1.x == p2.x && p1.y == p2.y - 1 ||
+        p1.x == p2.x && p1.y == p2.y + 1 ||
+        p1.y == p2.y && p1.x == p2.x - 1 ||
+        p1.y == p2.y && p1.x == p2.x + 1;
 }
